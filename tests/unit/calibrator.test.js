65d734d30c1a46f933de8fbd6d5ec2266cf97827
Daniel Stahl <danstahl1138@gmail.com>
2018-09-23 13:57:33 -0500

    deleted some stuff

--- tests/unit/calibrator.test.js
+++ tests/unit/calibrator.test.js
@@ -1,46 +1,6 @@
 const handler=require('../../lambda/calibrator')
-const parse = require('csv-parse')
-const parser=parse({
-    delimiter:',', 
-    cast:true, cast_date:true,
-    columns:true
-})
-const spline=require('cubic-spline')
-const fs=require('fs')
-const yieldCurve=require('./yield_curve.json')
-const asOfDate=Date.parse('2018-09-21')
-const asset=217.66
-const getIntegerArrayBetweenToPoints=(min, max)=>{
-    let arr=[]
-    for(let i=min; i<=max; ++i){
-      arr.push(i)
-    }
-    return arr
-}
-const yearsToMuturityForSpline=getIntegerArrayBetweenToPoints(0, 30)
 
-const ratioForUnixAndJSTimeStamp=1000
-
-const numMSInYears=24*60*60*365*ratioForUnixAndJSTimeStamp
-
-const getPriceFromBidAsk=({bid, ask})=>(bid+ask)*.5
-const getAnnualCurve=data=>data.dataset.data[0]
-const zeroCurveAtMaturities=(maturitiesInYears, annualCurve)=>{
-    annualCurve[0]=0
-    const updateAnnualCurve=annualCurve
-        .map(
-            (v, index)=>v*yearsToMuturityForSpline[index]
-        )
-    return maturitiesInYears.map(
-        maturityInYears=>spline(
-            maturityInYears, 
-            yearsToMuturityForSpline, 
-            updateAnnualCurve
-        )/maturityInYears
-    )
-}
-const convertPercentToNumber=val=>val*.01
-const convertPercentagesToNumber=vals=>vals.map(convertPercentToNumber)
+let optionPrices=require('./results.json')
 
 const createEvent=(data, parameters, queryStringParameters)=>({
     body:JSON.stringify(data),
@@ -49,79 +9,27 @@ const createEvent=(data, parameters, queryStringParameters)=>({
 })
 
 it('correctly calls calibrator handler for full model', (done)=>{
-    const annualCurve=convertPercentagesToNumber(getAnnualCurve(yieldCurve))
-    const input=fs.createReadStream('./tests/unit/apploptions.csv')
-    let results=[]
-    input.pipe(parser).on('data', row=>{
-        const updateRow={...row, spread:row.ask-row.bid, price:getPriceFromBidAsk(row)}
-        results.push(updateRow)
-    }).on('end', ()=>{
-        console.log(results)
-        const bestOptions=results.reduce((aggr, {maturity, strike, price, spread})=>{
-            const maturityInt=maturity.getTime()
-            if(!aggr[maturityInt]){
-                return {
-                    ...aggr,
-                    [maturityInt]:{
-                        [strike]:{
-                            price,
-                            spread
-                        }
-                    }
-                }
-            }
-            else if(!aggr[maturityInt][strike]||aggr[maturityInt][strike].spread>spread){
-                return {
-                    ...aggr,
-                    [maturityInt]:{
-                        ...aggr[maturityInt],
-                        [strike]:{
-                            price,
-                            spread
-                        }
-                    }
-                }
-            }
-            return aggr
-        }, {})
-        const maturities=Object.keys(bestOptions).filter((_v, i)=>i>0) //ignore the first one which is super close
-        const maturitiesInYears=maturities.map(maturity=>(new Date(parseInt(maturity))-asOfDate)/numMSInYears)
-        console.log(maturitiesInYears)
-        const zeroCurve=zeroCurveAtMaturities(maturitiesInYears, annualCurve)
-        const prettyResults=maturities.reduce((aggr, maturity, index)=>{
-            const priceAndStrike=Object.keys(bestOptions[maturity]).map(strike=>({
-                strike:parseFloat(strike),
-                price:bestOptions[maturity][strike].price
-            }))
-            return [...aggr, {
-                options:priceAndStrike,
-                maturity:maturitiesInYears[index],
-                rate:zeroCurve[index]            
-            }]
-        }, [])
-        fs.writeFile("./tests/unit/results.json", JSON.stringify({
-            options_and_rate:prettyResults,
-            asset}), err=>{
-            done()
-        })
-        //console.log(prettyResults)
-        /*const event=createEvent({
-            options_and_rate:prettyResults,
-            asset,
-            constraints:{}
-        }, {model:'cgmy'})
-        handler.calibrator(event, {}, (_err, val)=>{
-            console.log(val.body)
-            const parsedVal=JSON.parse(val.body).optimal_parameters
-            expect(parsedVal.sigma).toBeDefined()
-            expect(parsedVal.speed).toBeDefined()
-            expect(parsedVal.eta_v).toBeDefined()
-            expect(parsedVal.rho).toBeDefined()
-            console.timeEnd("calibrator")
-            done()
-        })*/
-        //done()
+               //console.log(prettyResults)
+    optionPrices.options_and_rate.forEach(v=>{
+        v.options=v.options.filter(({price})=>price>0)
+        v.options.sort((a, b)=>a.strike-b.strike)
     })
+    const event=createEvent({
+        ...optionPrices,
+        constraints:{}
+    }, {model:'merton_leverage'})
+    handler.calibrator(event, {}, (_err, val)=>{
+        console.log(val.body)
+        const parsedVal=JSON.parse(val.body).optimal_parameters
+        expect(parsedVal.sigma).toBeDefined()
+        expect(parsedVal.speed).toBeDefined()
+        expect(parsedVal.eta_v).toBeDefined()
+        expect(parsedVal.rho).toBeDefined()
+        console.timeEnd("calibrator")
+        done()
+    })
+        //done()
+    
     
    /* console.time("calibrator")
     const event=createEvent(calibratorParams, {model:'merton_leverage'})
