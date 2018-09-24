3b0b9e30a7c998135296fef6db383e8754aceedc
Daniel Stahl <daniel.stahl@regions.com>
2018-09-24 11:46:08 -0500

    improved schema

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-const {getOptionPrices}=require('./lambda/marketData')
-getOptionPrices({pathParameters:{ticker:'AAPL'}}, null, (_, res)=>{
-    console.log(res)
-})
+const toOpenApi = require('json-schema-to-openapi-schema')
+const fs=require('fs')
+const names=[
+    {
+        from:'GetConstraintsResponse_schema.json',
+        to:'GetConstraintsResponse.json'
+    },
+    {
+        from:'PostDensityResponse_schema.json',
+        to:'PostDensityResponse.json'
+    },
+    {
+        from:'PostOptionsRequest_schema.json',
+        to:'PostOptionsRequest.json'
+    },
+    {
+        from:'PostOptionsResponse_schema.json',
+        to:'PostOptionsResponse.json'
+    },
+    {
+        from:'PostRiskMetricResponse_schema.json',
+        to:'PostRiskMetricResponse.json'
+    }
+]
+names.forEach(({from, to})=>{
+    fs.readFile(`./models/${from}`, (err, data)=>{
+        if(err) return console.log(err)
+        const schema=JSON.parse(data)
+        const openApiSchema=toOpenApi(schema)
+        fs.writeFile(`./models/${to}`, JSON.stringify(openApiSchema), err=>{
+            if(err) console.log(err)
+        })
+    })
+}) 
+
