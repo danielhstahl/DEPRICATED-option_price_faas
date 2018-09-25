5af1f74bb96ba72ddea62544c376552bc83d0348
Daniel Stahl <danstahl1138@gmail.com>
2018-09-25 18:52:14 -0500

    should be working

--- main_site/src/Swagger.js
+++ main_site/src/Swagger.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import 'swagger-ui/dist/swagger-ui.css'
 import SwaggerUI from 'swagger-ui'
-import GitCommit from './_git_commit'
 export default class SwaggerTest extends Component {
   componentDidMount() {
     SwaggerUI({
       dom_id: '#swaggerContainer',
-      url:
-        `https://cdn.rawgit.com/phillyfan1138/option_price_faas/${GitCommit.tag}/docs/openapi_merged.yml`
+      url: `https://cdn.rawgit.com/phillyfan1138/option_price_faas/${
+        process.env.REACT_APP_TAG
+      }/docs/openapi_merged.yml`
       //presets: [presets.apis],
     })
   }
