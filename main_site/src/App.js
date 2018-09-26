616004bc6bc01e36e39dd03af1f7495fb4ee8bf7
Daniel Stahl <B0GX3@regions.com>
2018-09-26 09:59:43 -0500

    improved main site and fixed examples

--- main_site/src/App.js
+++ main_site/src/App.js
@@ -1,80 +1,40 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu } from 'antd'
 import Swagger from './Swagger'
+import FrontPage from './FrontPage'
 import './App.css'
 import 'antd/dist/antd.css'
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom'
 
-const { Content, Footer } = Layout
-const standardLight = '#fff'
-const standardDark = '#001529'
-const divDark = { background: standardDark, height: '100vh' }
-const divLight = { background: standardLight, height: '100vh' }
-const standardPadding = { padding: 100 }
+const { Content, Footer, Header } = Layout
+const lineHeight={lineHeight:'64px'}
 const App = () => (
   <Router>
-    <div>
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <Layout>
-            <Content>
-              <div style={divDark} id="home">
-                <h1 style={{ color: standardLight, ...standardPadding }}>
-                  Derivatives Modeling as a Service
-                </h1>
-                <p style={{ color: standardLight, ...standardPadding }}>
-                  For decades, the same financial models have been programmed
-                  and re-programmed at every bank. We are changing that.
-                  Combining state-of-the-art modeling with modern REST APIs, our
-                  models as a service provides robust, scalable infrastructure
-                  at a bargain price.
-                </p>
-              </div>
-              <div style={{ ...standardPadding, ...divLight }} id="features">
-                <h1>Option Pricing Models</h1>
-                <p>
-                  Our models are the most sophisticated in the industry. Our
-                  software engineering is top notch.
-                </p>
-                <ul>
-                  <li>Robust Black-Scholes implied volatility</li>
-                  <li>
-                    Modern calibration techniques using advanced models which go
-                    far beyond Heston.
-                  </li>
-                  <li>
-                    Modern calculation techniques including pricing, Greeks,
-                    option implied density, value at risk, and expected
-                    shortfall.
-                  </li>
-                </ul>
-                <p>
-                  For more information on the models, see the
-                  <a href={`${process.env.PUBLIC_URL}/OptionCalculation.pdf`}>
-                    summary documentation
-                  </a>
-                  and the detailed
-                  <a href={`${process.env.PUBLIC_URL}/OptionCalibration.pdf`}>
-                    calibration documentation
-                  </a>
-                </p>
-              </div>
-              <div style={divDark} id="pricing">
-                Pricing
-              </div>
-              <div style={divLight} id="about">
-                About
-                <Link to="/api_docs">Api Docs</Link>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>optsaas</Footer>
-          </Layout>
-        )}
-      />
-      <Route path="/api_docs" component={Swagger} />
-    </div>
+    <Layout>
+      <Redirect from='/' exact to='/home' />
+      <Header>
+        <Route path='/:page' render={({match})=>(
+          <Menu
+              mode="horizontal"
+              theme="dark"
+              selectedKeys={[match.params.page]}
+              style={lineHeight}
+          >
+            <Menu.Item key='home'><Link to='/home'>Home</Link></Menu.Item>
+            <Menu.Item key='api_docs'><Link to='/api_docs'>API Docs</Link></Menu.Item>
+          </Menu>
+        )} />          
+      </Header>
+      <Content>
+        <Route
+          exact
+          path="/home"
+          component={FrontPage}
+        />
+        <Route path="/api_docs" component={Swagger} />
+      </Content>
+      <Footer></Footer>
+    </Layout>
   </Router>
 )
 
---
+++ main_site/src/FrontPage.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import {  Row, Col } from 'antd'
+import './App.css'
+import 'antd/dist/antd.css'
+import { Link } from 'react-router-dom'
+
+const standardLight = '#fff'
+const standardDark = '#001529'
+const divDark = { background: standardDark, height: '100vh' }
+const divLight = { background: standardLight, height: '100vh' }
+const hLight={ color: standardLight }
+const sm={span:22, offset:1}
+const md={span:16, offset:2}
+const lg={span:16, offset:3}
+
+export default ()=>[
+    <Row style={divDark} gutter={16} key='summary'>
+        <Col sm={sm} md={md} lg={lg} style={{paddingTop:10}}>
+        
+            <h1 style={hLight}>
+                Derivatives Modeling as a Service
+            </h1>
+            <p style={hLight}>
+                For decades, the same financial models have been programmed
+                and re-programmed at every bank. We are changing that.
+                Combining state-of-the-art modeling with modern REST APIs, our
+                models as a service provides robust, scalable infrastructure
+                at a bargain price.
+            </p>
+        
+        </Col>
+    </Row>,
+    <Row style={divLight} gutter={16} key='description'>
+        <Col sm={sm} md={md} lg={lg}>
+            <h1>Option Pricing Models</h1>
+            <p>
+            Our models are the most sophisticated in the industry. Our
+            software engineering is top notch.
+            </p>
+            <ul>
+            <li>Robust Black-Scholes implied volatility</li>
+            <li>
+                Modern calibration techniques using advanced models which go
+                far beyond Heston.
+            </li>
+            <li>
+                Modern calculation techniques including pricing, Greeks,
+                option implied density, value at risk, and expected
+                shortfall.
+            </li>
+            </ul>
+            <p>
+            For more information on the models, see the
+            <a href={`${process.env.PUBLIC_URL}/OptionCalculation.pdf`}>
+                summary documentation
+            </a>
+            and the detailed
+            <a href={`${process.env.PUBLIC_URL}/OptionCalibration.pdf`}>
+                calibration documentation
+            </a>
+            </p>
+        </Col>
+    </Row>,
+    <Row style={divDark} gutter={16} key='pricing'>
+        <Col  sm={sm} md={md} lg={lg} style={hLight}>
+            Pricing
+        </Col>
+    </Row>,
+    <Row style={divLight} gutter={16} key='about'>
+        <Col  sm={sm} md={md} lg={lg}>
+            About
+            <Link to="/api_docs">Api Docs</Link>
+        </Col>
+    </Row>
+]
