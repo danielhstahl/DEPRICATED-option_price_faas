f65caf5a124161ee0afca6e8712ae6cd045c05f4
Daniel Stahl <B0GX3@regions.com>
2018-09-26 17:52:46 -0500

    improved routes

--- main_site/src/App.js
+++ main_site/src/App.js
@@ -4,26 +4,30 @@ import Swagger from './Swagger'
 import FrontPage from './FrontPage'
 import './App.css'
 import 'antd/dist/antd.css'
-import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
 
 const { Content, Footer, Header } = Layout
 const lineHeight={lineHeight:'64px'}
+const AppMenu=({match:{params:{page}}})=>(
+  <Menu
+      mode="horizontal"
+      theme="dark"
+      selectedKeys={[page]}
+      style={lineHeight}
+  >
+    <Menu.Item key='home'><Link to='/home'>Home</Link></Menu.Item>
+    <Menu.Item key='api_docs'><Link to='/api_docs'>API Docs</Link></Menu.Item>
+  </Menu>
+)
 const App = () => (
   <Router>
     <Layout>
-      <Redirect from='/' exact to='/home' />
+      
       <Header>
-        <Route path='/:page' render={({match})=>(
-          <Menu
-              mode="horizontal"
-              theme="dark"
-              selectedKeys={[match.params.page]}
-              style={lineHeight}
-          >
-            <Menu.Item key='home'><Link to='/home'>Home</Link></Menu.Item>
-            <Menu.Item key='api_docs'><Link to='/api_docs'>API Docs</Link></Menu.Item>
-          </Menu>
-        )} />          
+        <Switch>
+          <Redirect from='/' exact to='/home' />
+          <Route path='/:page' component={AppMenu} />     
+        </Switch>     
       </Header>
       <Content>
         <Route
