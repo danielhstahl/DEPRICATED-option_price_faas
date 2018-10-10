fb2af6de1db49fe6a41ea1e38a42c9dfc7eb926f
Daniel Stahl <B0GX3@regions.com>
2018-10-10 09:59:10 -0500

    improved products page

--- main_site/src/pages/Products.js
+++ main_site/src/pages/Products.js
@@ -5,19 +5,20 @@ import {
 import Pricing from '../components/Pricing'
 import {Link} from 'react-router-dom'
 const padding={paddingTop:40, paddingBottom:20}
-const mainSm={offset:3, size:6}
+const githubUrl='https://github.com/phillyfan1138'
+const documentationUrl=`${process.env.PUBLIC_URL}/OptionCalculation.pdf`
 export default ()=>(
 <Container>
     <Row key='description'>
-        <Col xs={12} sm={mainSm} style={padding}>
-            <h1 className='text-center'>Real Options</h1>
-            <p>Our flagship product!  Computes call and put prices, Black Scholes implied volatilities, value at risk, densities, and expected shortfall for three cutting edge models.</p>
+        <Col xs={12} sm={9} style={padding}>
+            <h1 className='display-4'>Real Options</h1>
+            <p >Our flagship product!  Computes call and put prices, Black Scholes implied volatilities, value at risk, densities, and expected shortfall for three cutting edge models.</p>
         </Col>
     </Row>
     <Row>
         <Col xs={12} sm={6}>
             <h3>Cutting edge models</h3>
-            <p>Heston, Extended Merton Jump-Diffusion, and Extended CGMY!  Each model includes a diffusion component which is correlated with a stochastic clock.  Incorporates skew, excess kurtosis, and stochastic volatility!</p>
+            <p>Heston, Extended Merton Jump-Diffusion, and Extended CGMY!  Each model includes a diffusion component which is correlated with a stochastic clock.  Incorporates skew, excess kurtosis, leverage effect, and stochastic volatility!</p>
         </Col>
         <Col xs={12} sm={6}>
             <h3>Easy to use</h3>
@@ -27,16 +28,18 @@ export default ()=>(
     <Row>
         <Col xs={12} sm={6}>
             <h3>Open and tested</h3>
-            <p>We extensively test our products for accuracy.  <a href='https://github.com/phillyfan1138'
-            target='_blank'
-            rel="noopener noreferrer">All our code is open source</a> so you can review the code yourself!</p>
+            <p>We extensively test our products for accuracy.  <a 
+                href={githubUrl}
+                target='_blank'
+                rel="noopener noreferrer"
+            >All our code is open source</a> so you can review the code yourself!</p>
         </Col>
         <Col xs={12} sm={6}>
             <h3>Documented</h3>
             <p>No option models are complete without documentation!  We provide comprehensive <a 
-            href={`${process.env.PUBLIC_URL}/OptionCalculation.pdf`}
-            target='_blank'
-            rel="noopener noreferrer"
+                href={documentationUrl}
+                target='_blank'
+                rel="noopener noreferrer"
             >documentation</a> for the model theory and assumptions.</p>
         </Col>
     </Row>
