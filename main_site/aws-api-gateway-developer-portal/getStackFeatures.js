916841811000fc640744facbea230a21747b7460
Daniel Stahl <danstahl1138@gmail.com>
2018-10-14 09:03:19 -0500

    updated client to reflect environment variables

--- main_site/aws-api-gateway-developer-portal/getStackFeatures.js
+++ main_site/aws-api-gateway-developer-portal/getStackFeatures.js
@@ -1,6 +1,7 @@
 const { exec } = require('child_process')
 const {writeFile} = require('fs') 
 const stackName=process.argv[2]
+const reactOutput=(key, value)=>'REACT_'+key+':'+value
 exec(
     `aws cloudformation describe-stacks --stack-name "${stackName}"`, 
     (err, stdout, stderr)=>{
@@ -9,7 +10,9 @@ exec(
             return console.log(error)
         }
         const result=JSON.parse(stdout).Stacks.find(({StackName})=>StackName===stackName)
-        const envStr=result.Outputs.reduce((aggr, {OutputKey, OutputValue})=>[...aggr, 'REACT_'+OutputKey+':'+OutputValue], []).join('\n')
+        const envArr1=result.Outputs.reduce((aggr, {OutputKey, OutputValue})=>[...aggr, reactOutput(OutputKey, OutputValue)], [])
+        const envArr2=result.Parameters.reduce((aggr, {ParameterValue, ParameterKey})=>[...aggr, reactOutput(ParameterKey, ParameterValue)], [])
+        const envStr=[...envArr1, ...envArr2].join('\n')
         writeFile('./.env', envStr, err=>{
             if(err){
                 console.log(err)
