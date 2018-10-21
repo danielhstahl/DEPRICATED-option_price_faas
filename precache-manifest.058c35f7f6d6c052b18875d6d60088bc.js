0a8453d697d8a6ce28997020f1f793f15ff69f27
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-21 17:30:07 +0000

    Deploy phillyfan1138/option_price_faas to github.com/phillyfan1138/option_price_faas.git:gh-pages

---
+++ precache-manifest.058c35f7f6d6c052b18875d6d60088bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cab8797019086a09288",
+    "url": "/static/js/main.6cab8797.chunk.js"
+  },
+  {
+    "revision": "ff156d34dd7fd88982eb",
+    "url": "/static/js/1.ff156d34.chunk.js"
+  },
+  {
+    "revision": "6cab8797019086a09288",
+    "url": "/static/css/main.c94131a6.chunk.css"
+  },
+  {
+    "revision": "ff156d34dd7fd88982eb",
+    "url": "/static/css/1.10e6db0d.chunk.css"
+  },
+  {
+    "revision": "dd3b8287dcb3bcf0c98bb9cff04c516d",
+    "url": "/index.html"
+  }
+];
