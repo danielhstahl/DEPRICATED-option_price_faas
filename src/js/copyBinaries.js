cffac35605643ad343f8b89dc5287b1ba47cfd16
Daniel Stahl <daniel.stahl@regions.com>
2018-11-30 16:42:10 -0600

    fixed binaries

--- src/js/copyBinaries.js
+++ src/js/copyBinaries.js
@@ -1,22 +1,19 @@
 const fs=require('fs-extra')
 const origBinaryLocation='./target/x86_64-unknown-linux-musl/release'
 const newBinaryLocation='./target/lambda'
-
-module.exports=()=>Promise.all([
-    fs.copy(
-        `${origBinaryLocation}/output_constraints`, 
-        `${newBinaryLocation}/constraints/bootstrap`
-    ),
-    fs.copy(
-        `${origBinaryLocation}/pricer`, 
-        `${newBinaryLocation}/pricer/bootstrap`
-    ),
-    fs.copy(
-        `${origBinaryLocation}/riskmetric`, 
-        `${newBinaryLocation}/riskmetric/bootstrap`
-    ),
-    fs.copy(
-        `${origBinaryLocation}/density`, 
-        `${newBinaryLocation}/density/bootstrap`
-    )
-])
+const locationNames=[
+    'constraints',
+    'pricer',
+    'riskmetric',
+    'density'
+]
+module.exports=()=>fs.ensureDir(newBinaryLocation)
+    .then(()=>Promise.all(
+        locationNames.map(name=>fs.ensureDir(`${newBinaryLocation}/${name}`))
+    ))
+    .then(()=>Promise.all(
+        locationNames.map(name=>fs.copy(
+            `${origBinaryLocation}/${name}`, 
+            `${newBinaryLocation}/${name}/bootstrap`
+        ))
+    ))
