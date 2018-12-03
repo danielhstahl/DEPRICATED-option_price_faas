c64a5010eb9c21f2e64446a5919f5c557ecef051
Daniel Stahl <danstahl1138@gmail.com>
2018-12-02 18:39:21 -0600

    fixed permissions issues

--- src/js/copyBinaries.js
+++ src/js/copyBinaries.js
@@ -1,8 +1,9 @@
 const fs=require('fs-extra')
 const admZip=require('adm-zip')
 const zip=new admZip()
+const {exec}=require('child_process')
 const origBinaryLocation='./target/x86_64-unknown-linux-musl/release'
-const newZipLocation='./'
+const newZipLocation='.'
 const locationNames=[
     'constraints',
     'pricer',
@@ -10,15 +11,23 @@ const locationNames=[
     'density'
 ]
 module.exports=()=>locationNames.forEach(name=>{
-    fs.ensureDir(`${newZipLocation}/${name}`).then(()=>{
-        return fs.copy(
-            `${origBinaryLocation}/${name}`,
-            `${newZipLocation}/${name}/bootstrap`
-        )
-    }).then(()=>{
+    fs.ensureDir(`${newZipLocation}/${name}`)
+    .then(()=>{
+        return new Promise((res, rej)=>{
+            exec(`cp -a ${origBinaryLocation}/${name} ${newZipLocation}/${name}/bootstrap`, (err, stdout, stderr)=>{
+                const error=err||stderr
+                if(error){
+                    return rej(error)
+                }
+                return res(stdout)
+            })
+        })
+    })
+    .then(()=>{
         zip.addLocalFile(`${newZipLocation}/${name}/bootstrap`)
         zip.writeZip(`${newZipLocation}/${name}.zip`)
-    }).then(()=>{
+    })
+    .then(()=>{
         return fs.remove(`${newZipLocation}/${name}`)
     })
 })
