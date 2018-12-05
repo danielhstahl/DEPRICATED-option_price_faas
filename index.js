0274c2fc8d838869fb564a4a18c09b5d532e4e9c
Daniel Stahl <daniel.stahl@regions.com>
2018-12-05 09:02:48 -0600

    added lock and cleanup

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-//const binaryCopy=require('./src/js/copyBinaries')
 const yamlWrite=require('./src/js/writeYml')
 
 yamlWrite().catch(err=>{
     console.log(err)
 })
-
-/*Promise.all([
-    binaryCopy(),
-    yamlWrite()
-]).then(([resb, resy])=>{
-    console.log(resb)
-    console.log(resy)
-}).catch(err=>{
-    console.log(err)
-})*/
